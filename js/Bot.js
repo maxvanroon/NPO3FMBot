27282e719d2d247137131c381e4b2d2214dd939c
maxvanroon <maxvanroon10@hotmail.com>
2019-06-01 12:10:26 +0200

    Update Bot.js

--- js/Bot.js
+++ js/Bot.js
@@ -71,8 +71,8 @@ Bot.login = function() {
 Bot.onReady = function() {
 	if(process.send) process.send('BotReady');
 	console.log('Bot is ready!');
-	bot.user.setActivity("NPO 3FM", {type: "LISTENING"});
 	this.preformInitialization();
+	bot.user.setActivity("NPO 3FM");
 };
 
 Bot.preformInitialization = function() {
